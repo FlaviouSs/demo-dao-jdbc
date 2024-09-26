ab7c503d27520f774141f2543b714628fb43eb5d
Flavio Alecio <flaviotubaroni@gmail.com>
2024-09-26 10:54:07 -0300

    findById implementation

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,12 +1,18 @@
 package application;
 
-import model.entities.Department;
+import model.dao.DAOFactory;
+import model.dao.SellerDAO;
+import model.entities.Seller;
 
 public class Program {
 
 	public static void main(String[] args) {
-		Department obj = new Department(1, "Books");
-		System.out.println(obj);
+		
+		SellerDAO sellerDAO = DAOFactory.createSellerDAO();
+		
+		Seller seller = sellerDAO.findById(12);
+		
+		System.out.println(seller);
 
 	}
 
--- src/model/dao/impl/SellerDAO_JDBC.java
+++ src/model/dao/impl/SellerDAO_JDBC.java
@@ -1,5 +1,85 @@
 package model.dao.impl;
 
-public class SellerDAO_JDBC {
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.List;
+
+import db.DB;
+import db.DBException;
+import model.dao.SellerDAO;
+import model.entities.Department;
+import model.entities.Seller;
+
+public class SellerDAO_JDBC implements SellerDAO{
+
+	private Connection conn = null;
+	
+	public SellerDAO_JDBC(Connection conn) {
+		this.conn = conn;
+	}
+	
+	@Override
+	public void insert(Seller obj) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void update(Seller obj) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void deleteById(Integer id) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public Seller findById(Integer id) {
+		PreparedStatement st = null;
+		ResultSet rs = null;
+		try {
+			st = conn.prepareStatement(
+					"SELECT s.id, s.name, s.email, s.birth_date, s.base_salary, s.department_id, d.name as DepName FROM seller s"
+					+ " JOIN department d ON s.department_id = d.id WHERE s.id = ?;");
+			
+			st.setInt(1, id);
+			
+			rs = st.executeQuery();
+			
+			if(rs.next()) {
+				Department dep = new Department();
+				dep.setId(rs.getInt("department_id"));
+				dep.setName(rs.getString("DepName"));
+				Seller obj = new Seller();
+				obj.setId(rs.getInt("id"));
+				obj.setName(rs.getString("name"));
+				obj.setEmail(rs.getString("email"));
+				obj.setBaseSalary(rs.getDouble("base_salary"));
+				obj.setBirthDate(rs.getDate("birth_date"));
+				obj.setDepartment(dep);
+				return obj;
+			}
+			
+			return null;
+		}
+		catch(SQLException e) {
+			throw new DBException(e.getMessage());
+		}
+		finally {
+			DB.closeStatement(st);
+			DB.closeResultSet(rs);
+		}
+	}
+
+	@Override
+	public List<Seller> findAll() {
+		// TODO Auto-generated method stub
+		return null;
+	}
 
 }
